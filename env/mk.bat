816c989598076fe5b74bfbbe585bf116fb7eed29
terraritto <28126083+terraritto@users.noreply.github.com>
2024-08-16 23:54:52 +0900

    ・Kernelに対応するように

--- env/mk.bat
+++ env/mk.bat
@@ -1,1 +1,8 @@
-@nasm boot.asm -o boot.img -l boot.lst
+@echo off
+if exist kernel.asm (
+    nasm boot.asm -o boot.bin -l boot.lst
+    nasm kernel.asm -o kernel.bin -l kernel.lst
+    copy /B boot.bin+kernel.bin boot.img
+) else (
+    nasm boot.asm -o boot.bin -l boot.lst
+)
